767ae463a58d33e1547be4fe108d95febcfd8d8b
ang <809198058@qq.com>
2019-12-05 20:34:03 +0800

    添加：云函数 flowTaskLogList 中添加获取评论列表函数，并且修改promise部分逻辑代码

--- sudoList/cloud_function/followTaskLogList/index.js
+++ sudoList/cloud_function/followTaskLogList/index.js
@@ -6,6 +6,20 @@ cloud.init()
 const db = cloud.database();
 const _ = db.command
 
+// 获取打卡评论列表
+const getTaskLogListCommentList = (taskLogId) => {
+  return db.collection("comment").where({
+    taskLogId: taskLogId
+  }).orderBy("ctime", "desc").get()
+}
+// 获取用户信息
+const getUserInfo = (open_id) => {
+  return db.collection("user_info").where({
+    _open_id: open_id
+  }).get()
+}
+
+
 // 云函数入口函数
 exports.main = async(event, context) => {
   let {
@@ -21,29 +35,28 @@ exports.main = async(event, context) => {
   let followList = await db.collection("follow").where({
     _open_id: wxContext.OPENID
   }).get()
-  let followUserIdList = []
-  followList.data.forEach(item => {
-    followUserIdList.push(item.followOpenId)
+  let followUserIdList = followList.data.map(item => {
+    return item.followOpenId
   })
   // 获取所有关注用户最新日志列表
   let taskLogList = await db.collection("task_log").where({
     _open_id: _.in(followUserIdList)
   }).orderBy('ctime', 'desc').skip(page).limit(60).get()
   responseData.taskLogList = taskLogList.data
-  let getUserInfoArray = []
-  for (let i = 0; i < responseData.taskLogList.length; i++) {
-    let a = db.collection("user_info").where({
-      _open_id: responseData.taskLogList[i]._open_id
-    }).get().then(res => {
-      responseData.taskLogList[i].nickName = res.data[0].nickName
-      responseData.taskLogList[i].avatarUrl = res.data[0].avatarUrl
-      responseData.taskLogList[i].gender = res.data[0].gender
+
+  let userAndCommnetPromiseList = responseData.taskLogList.map(item => {
+    let getUserInfoPromise = getUserInfo(item._open_id).then(res => {
+      item.nickName = res.data[0].nickName
+      item.avatarUrl = res.data[0].avatarUrl
+      item.gender = res.data[0].gender
     })
-    getUserInfoArray.push(a)
-  }
-  await Promise.all(getUserInfoArray).then(res=>{
-    console.log(res)
+    let getTaskLogListCommentListPromise = getTaskLogListCommentList(item.taskLogId).then(res => {
+      item.commentList = res.data
+    })
+
+    return Promise.all(getUserInfoPromise, getTaskLogListCommentListPromise)
   })
+  await Promise.all(userAndCommnetPromiseList)
   let res = await cloud.callFunction({
     name: "comReturn",
     data: {
@@ -51,6 +64,5 @@ exports.main = async(event, context) => {
       data: responseData.taskLogList
     }
   })
-
   return res.result
 }
