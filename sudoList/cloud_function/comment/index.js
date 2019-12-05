e546b46e289d9a68792b62547991c38f71e6133b
ang <809198058@qq.com>
2019-12-05 19:25:15 +0800

    添加：云函数comment，新增评论时添加评论人姓名和回复者姓名字段

--- sudoList/cloud_function/comment/index.js
+++ sudoList/cloud_function/comment/index.js
@@ -4,15 +4,43 @@ const TABLE = "comment"
 const nowTs = Date.parse(new Date)
 cloud.init()
 const db = cloud.database()
+const _ = db.command
 
+// 获取用户信息
+const getUserInfo = (openId) => {
+  return db.collection("user_info").where({
+    _open_id: openId
+  }).get()
+}
+// 添加打卡日志评论数
+const addCommentNums = (taskLogId) => {
+  return db.collection("task_log").where({
+    _id: taskLogId
+  }).update({
+    data: {
+      commentNums: _.inc(1)
+    }
+  })
+}
+const AddComment = async(taskLogId, openId, toOpenId, commentText) => {
+  let sendNickName = ""
+  let toNickName = ""
+  let sendUserInfoPromise = getUserInfo(openId).then(res => {
+    sendNickName = res.data[0].nickName || ""
+  })
+  let toUserInfoPromise = getUserInfo(toOpenId).then(res => {
+    toNickName = res.data[0].nickName || ""
+  })
+  await Promise.all([addCommentNums(taskLogId), sendUserInfoPromise, toUserInfoPromise])
 
-const AddComment = (taskLogId, openId, toOpenId, commentText) => {
   return db.collection(TABLE).add({
     data: {
       "taskLogId": taskLogId,
       "_open_id": openId,
       "toOpenId": toOpenId,
       "commentText": commentText,
+      "sendNickName": sendNickName,
+      "toNickName": toNickName,
       "ctime": nowTs,
       "mtime": nowTs
     }
@@ -32,11 +60,17 @@ exports.main = async(event, context) => {
     paramDcit
   } = event
   const wxContext = cloud.getWXContext()
-  if(action == "Add"){
-    let {taskLogId, toOpenId, commentText} = paramDcit
+  if (action == "Add") {
+    let {
+      taskLogId,
+      toOpenId,
+      commentText
+    } = paramDcit
     return await AddComment(taskLogId, wxContext.OPENID, toOpenId, commentText)
-  }else{
-    let {commentId} = paramDcit
+  } else {
+    let {
+      commentId
+    } = paramDcit
     return await DelComment(commentId)
   }
 }
