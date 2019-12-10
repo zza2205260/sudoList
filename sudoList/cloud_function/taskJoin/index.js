f02b0b8dad8a16347b49be1e25f8fe1de6cad4dc
ang <809198058@qq.com>
2019-12-11 00:21:46 +0800

    修改：修改加入习惯逻辑

--- sudoList/cloud_function/taskJoin/index.js
+++ sudoList/cloud_function/taskJoin/index.js
@@ -4,32 +4,43 @@ const cloud = require('wx-server-sdk')
 cloud.init()
 
 const db = cloud.database()
-const errorTaskExist = [-200, "已经加入该圈子了！"]
+const errorTaskNotExist = [-201, "该习惯已经消失！"]
+const errorTaskExist = [-200, "已经加入该习惯了！"]
 
+const nowTs = Date.now()
+const joinTask = (taskId, openId) => {
+  return db.collection("task_join_user").add({
+    data:{
+      taskId: taskId,
+      _open_id: openId,
+      ctime: nowTs,
+      mtime: nowTs
+    }
+  })
+}
 // 云函数入口函数
 exports.main = async(event, context) => {
   const {
-    taskId,
-    title
+    taskId
   } = event;
   const wxContext = cloud.getWXContext()
   let error = null
-  const nowTs = Date.now()
+
   let task = await db.collection("task").where({
-    _id: taskId,
-    _open_id: wxContext.OPENID
-  }).get()
-  if (task.data.length == 0) {
-    db.collection("task").add({
-      data: {
-        _open_id: wxContext.OPENID,
-        title: title,
-        ctime: nowTs,
-        mtime: nowTs
-      }
-    })
+    _id: taskId
+  }).count()
+  if (task.total != 0) {
+    let joinNum = await db.collection("task_join_user").where({
+      taskId: taskId,
+      _open_id: wxContext.OPENID
+    }).count()
+    if (joinNum.total == 0) {
+      await joinTask(taskId, wxContext.OPENID)
+    } else {
+      error = errorTaskExist
+    }
   } else {
-    error = errorTaskExist
+    error = errorTaskNotExist
   }
   let res = await cloud.callFunction({
     name: "comReturn",
