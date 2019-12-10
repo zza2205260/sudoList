95a974cb0b27aff41e0280e7171ac83ff1d86722
ang <809198058@qq.com>
2019-12-11 00:20:14 +0800

    添加：添加用户加入某个习惯的逻辑

--- sudoList/cloud_function/taskAdd/index.js
+++ sudoList/cloud_function/taskAdd/index.js
@@ -3,14 +3,29 @@ const cloud = require('wx-server-sdk')
 
 cloud.init()
 const db = cloud.database();
+const nowTs = Date.parse(new Date())
+
+const addTaskUserJoin = (taskId, openId) => {
+  return db.collection("task_join_user").add({
+    data: {
+      taskId: taskId,
+      _open_id: openId,
+      ctime: nowTs,
+      mtime: nowTs
+    }
+  })
+}
+
+
+
 // 云函数入口函数
-exports.main = async (event, context) => {
+exports.main = async(event, context) => {
   const wxContext = cloud.getWXContext()
   const responseData = {
     data: {}
   }
   let error = null;
-  let nowTs = Date.parse(new Date())
+
   let data = await db.collection('task').add({
     data: {
       _open_id: wxContext.OPENID,
@@ -19,6 +34,7 @@ exports.main = async (event, context) => {
       mtime: nowTs
     }
   })
+  await addTaskUserJoin(data._id, wxContext.OPENID)
   let res = await cloud.callFunction({
     name: 'comReturn',
     data: {
