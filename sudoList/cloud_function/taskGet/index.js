5325f3cc1f996519d8bd62b09c258f87cb95a62c
ang <809198058@qq.com>
2019-12-11 00:21:10 +0800

    修改：修改习惯加入人数逻辑，更新为通过表task_join_user来记录

--- sudoList/cloud_function/taskGet/index.js
+++ sudoList/cloud_function/taskGet/index.js
@@ -4,6 +4,13 @@ const cloud = require('wx-server-sdk')
 cloud.init()
 
 const db = cloud.database();
+
+const getTaskNum = (taskId) =>{
+  return db.collection("task_join_user").where({
+    taskId: taskId
+  }).count()
+}
+
 // 云函数入口函数
 exports.main = async (event, context) => {
   const responseData = {
@@ -16,7 +23,8 @@ exports.main = async (event, context) => {
   .get()
   if(taskDetails.data.length != 0){
     responseData.task = taskDetails.data[0]
-    responseData.task.joinNums = taskDetails.data.length
+    let taskNums = await getTaskNum(event.taskId)
+    responseData.task.joinNums = taskNums.total
   }
   const res = await cloud.callFunction({
     name: 'comReturn',
