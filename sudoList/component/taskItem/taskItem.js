5172a242c0753607313103ab6fa2d601612400d6
ang <809198058@qq.com>
2019-12-22 13:18:08 +0800

    新增：新增组件 taskItem

---
+++ sudoList/component/taskItem/taskItem.js
@@ -0,0 +1,26 @@
+// component/taskItem/taskItem.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    dataList : {
+      type: Array,
+      value: []
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
