266cb711ba1ca9f476110e119aea14d664dd6855
ang <809198058@qq.com>
2019-12-22 13:20:20 +0800

    新增：新增taskList云函数，其中获取热门习惯的方法。

--- sudoList/cloud_function/taskList/index.js
+++ sudoList/cloud_function/taskList/index.js
@@ -5,10 +5,36 @@ cloud.init()
 
 const db = cloud.database();
 
+const getHotTask = async() => {
+  let hotList = await db.collection("task_join_user").aggregate().sortByCount('$taskId').end()
+  let taskList = []
+  let promiseList = hotList.list.map(item => {
+    return getTask(item._id).then(res => {
+      let tempTask = res.data[0]
+      tempTask.count = item.count
+      taskList.push(tempTask)
+    })
+  })
+  await Promise.all(promiseList)
+  return taskList
+}
+const getTask = (taskId) => {
+  return db.collection("task").where({
+    _id: taskId
+  }).get()
+}
+const getUserTask = (openId) => {
+  return db.collection("task").where({
+    _open_id: openId
+  }).orderBy('ctime', 'asc').get()
+}
+
+
 // 云函数入口函数
 exports.main = async(event, context) => {
   const {
-    version
+    version,
+    type
   } = event
   const wxContext = cloud.getWXContext()
   let openId = wxContext.OPENID
@@ -17,18 +43,20 @@ exports.main = async(event, context) => {
   }).get()
   if (version == envData.data[0].version) {
     // openId = 'otMTI5bQF-Yk6f0peT_xhS-khRvA'
-    openId = 'otMTI5V_YR2X8SK6yelh_WTc4LZA'
+    openId = 'o83qX5JCrgDpDDF6COVkTig6RWIs'
   }
   const responseData = {
     taskList: []
   }
   let error = null;
-  let taskList = await db.collection('task')
-    .where({
-      _open_id: openId
-    })
-    .orderBy('ctime', 'asc').get()
-  responseData.taskList = taskList.data;
+  let taskList = null;
+  if (type == 'HOT') {
+    taskList = await getHotTask()
+    responseData.taskList = taskList
+  } else {
+    taskList = await getUserTask(openId)
+    responseData.taskList = taskList.data;
+  }
 
   let res = await cloud.callFunction({
     name: 'comReturn',
